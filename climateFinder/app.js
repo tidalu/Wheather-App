a7ab9e7a6fd374509cd0e7906b6c2532e5424d47
Tidalu <127156318+tidalu@users.noreply.github.com>
2023-03-25 02:30:51 +0100

    Add files via upload

--- climateFinder/app.js
+++ climateFinder/app.js
@@ -1,37 +1,51 @@
-let searchBtn = document.getElementById("searchBtn");
-let inputValue = document.getElementById("inputValue");
-let temp = document.getElementById("currentDegree");
-let humidity = document.getElementById("currentHumidity");
-let realFeel = document.getElementById("currentRealFeel");
-let windSpeed = document.getElementById("currentWindSpeed");
-let cityName = document.getElementById("currentCity");
 
+
+const searchBtn = document.getElementById("searchBtn"),
+      inputValue = document.getElementById("inputValue"),
+      temperature = document.getElementById("currentDegree"),
+      humidity = document.getElementById("currentHumidity"),
+      realFeel = document.getElementById("currentRealFeel"),
+      windSpeed = document.getElementById("currentWindSpeed"),
+      cityName = document.getElementById("currentCity"),
+      countryName = document.getElementById("currentCountry");
+    
+// Add event listener for click on search button
 searchBtn.addEventListener("click", function () {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=51a9ed3379cefdcccc1c0afc1b54ad01"
-  )
+  getWeatherData();
+});
+
+// Add event listener for keypress enter on input field
+inputValue.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    getWeatherData();
+  }
+});
+
+// Function to fetch weather data and update HTML
+function getWeatherData() {
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=51a9ed3379cefdcccc1c0afc1b54ad01`;
+  fetch(api)
     .then((response) => response.json())
     .then((data) => {
-      let tempValue = data["main"]["temp"];
-      let humidityValue = data["main"]["humidity"];
-      let realFeelValue = data["main"]["feels_like"];
-      let windSpeedValue = data["wind"]["speed"];
-      console.log(data);
-      let searchedCityName = data["name"];
+      let tempValue = data.main.temp;
+      let humidityValue = data.main.humidity;
+      let realFeelValue = data.main.feels_like;
+      let windSpeedValue = data.wind.speed;
+      let searchedCityName = data.name;
+      let countryShort = data.sys.country;
 
-      temp.innerHTML = Math.round(tempValue - 273.15);
+      temperature.innerHTML = Math.round(tempValue - 273.15);
       humidity.innerHTML = humidityValue;
       realFeel.innerHTML = Math.round(realFeelValue - 273.15);
       windSpeed.innerHTML = windSpeedValue;
       cityName.innerHTML = searchedCityName;
+      countryName.innerText = countryShort;
     })
-
     .catch((err) => {
-      alert("bla bla bla");
+      alert("Error");
       console.log(err);
     });
-  // .catch(err => alert("Wrong City Name!"))
-});
+}
 
 let btnChange = document.querySelector(".circleChange");
 let bodyColor = document.body;
